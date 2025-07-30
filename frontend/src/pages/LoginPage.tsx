1c1894bbd12b791949f4c9f599b132257ae75065
Dudas Timea <h257330@stud.u-szeged.hu>
2025-07-30 18:45:39 +0200

    fix: grammar fix and translation

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -11,7 +11,7 @@ export default function AuthPage() {
 
    const handleLogin = async () => {
     if (!username || !password) {
-      setLoginError('Username or password cannot be empty.');
+      setLoginError('Felhasználónév és jelszó mező kitöltése kötelező.');
       return;
     }
 
@@ -26,15 +26,15 @@ export default function AuthPage() {
     setLoginError(
       err.response?.data?.error ||
       (err.response?.data?.authenticated === false
-        ? 'Invalid username or password.'
-        : 'Something went wrong during login.')
+        ? 'Érvénytelen felhasználónév vagy jelszó.'
+        : 'Hiba történt a bejelentkezés során.')
     );
   }
 };
 
 const handleRegister = async () => {
     if (!username || !password) {
-      setRegisterError('Username and password cannot be empty.');
+      setRegisterError('Felhasználónév és jelszó mező kitöltése kötelező.');
       return;
     }
 
@@ -47,7 +47,7 @@ const handleRegister = async () => {
     console.error('Register failed:', err.response?.data || err.message);
     setRegisterError(
       err.response?.data?.error ||
-      'Something went wrong during registration.'
+      'Hiba történt a regisztráció során.'
     );
   }
 };
@@ -68,31 +68,31 @@ const handleRegister = async () => {
             <div className="w-1/2 flex flex-col lg:flex-row">
               <div className="w-1/2 hidden lg:flex items-center justify-center text-white text-center p-6">
                 <div>
-                  <h1 className="text-4xl font-extrabold mb-2">Welcome back!</h1>
-                  <p className="mb-10">Don't have an account yet? Join us and start snackin'!</p>
+                  <h1 className="text-4xl font-extrabold mb-2">Üdv újra!</h1>
+                  <p className="mb-10">Még nincs fiókod? Csatlakozz és kóstolj bele a lehetőségekbe!</p>
                   <button
                     onClick={() => setIsLogin(false)}
                     className="px-16 py-2 border border-white rounded-lg hover:bg-white/40 bg-white/20 transition"
                   >
-                    Register
+                    Regisztráció
                   </button>
                 </div>
               </div>
 
               <div className="w-full lg:w-1/2 flex items-center justify-center bg-white/80 p-8 z-10 gap-9">
-                <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }} className="w-full max-w-sm space-y-6">
-                  <h2 className="text-2xl font-bold">Login</h2>
-                  <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-2 border rounded" />
-                  <input type="password" value={password}  onChange={(e) => setPassword(e.target.value)} placeholder="Password" className="w-full p-2 border rounded" />
-                  <button type="submit" className="w-full border-dorange border-1 font-bold  rounded-lg text-white hover:bg-lorange/80 bg-lorange/100 p-2 mt-2 transform">Login</button>
-                  {loginError && <p className="text-red-600">{loginError}</p>}
+                <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }} className="w-full max-w-sm lg:space-y-6 space-y-2">
+                  <h2 className="text-2xl font-bold">Bejelentkezés</h2>
+                  <input type="text" required placeholder="Felhasználónév" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-2 border rounded" />
+                  <input type="password" required value={password}  onChange={(e) => setPassword(e.target.value)} placeholder="Jelszó" className="w-full p-2 border rounded" />
+                  <button type="submit" className="w-full border-dorange border-1 font-bold  rounded-lg text-white hover:bg-lorange/80 bg-lorange/100 p-2 mt-2 transform">Bejelentkezés</button>
+                  {loginError && <p className="text-red-400">{loginError}</p>}
                   <div className="lg:invisible md:invisible visible flex flex-row flex-1 justify-center flex-wrap">
                     <div className="flex items-center justify-center w-full gap-2">
                       <hr className="flex-grow h-px bg-gradient-to-r from-gray-400 to-gray-200 border-0" />
-                      <p className="text-gray-500 text-sm">or</p>
+                      <p className="text-gray-500 text-sm">vagy</p>
                       <hr className="flex-grow h-px bg-gradient-to-r from-gray-200 to-gray-400 border-0" />
                     </div>
-                    <button type="button" onClick={() => setIsLogin(false)} className="px-10 py-2 border border-gray-400 rounded-lg hover:bg-gray-600/30 bg-gray-400/10 transition mt-4">Register</button>
+                    <button type="button" onClick={() => setIsLogin(false)} className="px-10 py-2 border border-gray-400 rounded-lg hover:bg-gray-600/30 bg-gray-400/10 transition mt-4">Regisztráció</button>
                   </div>
 
                 </form>
@@ -101,31 +101,31 @@ const handleRegister = async () => {
 
             <div className="w-1/2 flex flex-col lg:flex-row">
               <div className="w-full lg:w-1/2 flex items-center justify-center bg-white/80 p-8 z-10 gap-6">
-                <form className="w-full max-w-sm space-y-6" onSubmit={(e) => { e.preventDefault(); handleRegister(); }}>
-                  <h2 className="text-2xl font-bold">Register</h2>
-                  <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-2 border rounded" />
-                  <input type="password" placeholder="Password"  value={password}  onChange={(e) => setPassword(e.target.value)} className="w-full p-2 border rounded" />
-                  <button type="submit" className="w-full border-dorange border-1 font-bold  rounded-lg text-white hover:bg-lorange/80 bg-lorange/100 p-2 mt-2 transform">Register</button>
-                   {registerError && <p className="text-red-600">{registerError}</p>}
+                <form className="w-full max-w-sm lg:space-y-6 space-y-2" onSubmit={(e) => { e.preventDefault(); handleRegister(); }}>
+                  <h2 className="text-2xl font-bold">Regisztráció</h2>
+                  <input type="text" placeholder="Felhasználónév" required value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-2 border rounded" />
+                  <input type="password" placeholder="Jelszó" required value={password}  onChange={(e) => setPassword(e.target.value)} className="w-full p-2 border rounded" />
+                  <button type="submit" className="w-full border-dorange border-1 font-bold  rounded-lg text-white hover:bg-lorange/80 bg-lorange/100 p-2 mt-2 transform">Regisztráció</button>
+                   {registerError && <p className="text-red-400">{registerError}</p>}
                    <div className="lg:invisible md:invisible visible flex flex-row flex-1 justify-center flex-wrap">
                       <div className="flex items-center justify-center w-full gap-2 ">
                         <hr className="flex-grow h-px bg-gradient-to-r from-gray-400 to-gray-200 border-0" />
-                        <p className="text-gray-500 text-sm">or</p>
+                        <p className="text-gray-500 text-sm">vagy</p>
                         <hr className="flex-grow h-px bg-gradient-to-r from-gray-200 to-gray-400 border-0" />
                       </div>
-                      <button type="button" onClick={() => setIsLogin(true)} className="px-10 py-2 border relative align-middle border-gray-400 rounded-lg hover:bg-gray-600/30 bg-gray-400/10 transition mt-4">Login</button>
+                      <button type="button" onClick={() => setIsLogin(true)} className="px-10 py-2 border relative align-middle border-gray-400 rounded-lg hover:bg-gray-600/30 bg-gray-400/10 transition mt-4">Bejelentkezés</button>
                     </div>
                 </form>
               </div>
               <div className="w-1/2 hidden lg:flex items-center justify-center text-white text-center p-6">
                 <div>
-                  <h1 className="text-4xl font-extrabold mb-2">Taste the World</h1>
-                  <p className="mb-10">Already have an account? Click below to log in.</p>
+                  <h1 className="text-4xl font-extrabold mb-2">Ízleld meg a világot!</h1>
+                  <p className="mb-10">Már van fiókod? Kattints a bejelentkezéshez.</p>
                   <button type="button"
                     onClick={() => setIsLogin(true)}
                     className="px-16 py-2 border border-white rounded-lg hover:bg-white/40 bg-white/20 transition"
                   >
-                    Login
+                    Bejelentkezés
                   </button>
                    
                 </div>
